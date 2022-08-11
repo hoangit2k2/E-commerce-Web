b66d6438840fc05d46d100acaa6c876962627782
hoangit2k2 <nguyenhoang2002it@gmail.com>
2022-08-11 23:10:58 +0700

    delete

--- Assignment_Java6/src/main/java/edu/hoang/DAO/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package edu.hoang.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.hoang.entity.Category;
-
-public interface CategoryDAO extends JpaRepository<Category,String> {
-	
-}
--- Assignment_Java6/src/main/java/edu/hoang/DAO/OrderDAO.java
+++
@@ -1,9 +0,0 @@
-package edu.hoang.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.hoang.entity.Order;
-
-public interface OrderDAO extends JpaRepository<Order,Long> {
-	
-}
