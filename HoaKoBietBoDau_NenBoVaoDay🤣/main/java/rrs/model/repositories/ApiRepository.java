55b56d1e64d5ef716f179b3b0ceab4ffcc8edb94
DuyHoa <87412532+Duyhoa2002@users.noreply.github.com>
2022-10-04 06:59:15 +0700

    Delete HoaKoBietBoDau_NenBoVaoDay🤣 directory

--- HoaKoBietBoDau_NenBoVaoDay🤣/main/java/rrs/model/repositories/ApiRepository.java
+++
@@ -1,9 +0,0 @@
-package rrs.model.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import rrs.model.entities.Api;
-
-@Repository
-public interface ApiRepository extends JpaRepository<Api, String>{
-}
