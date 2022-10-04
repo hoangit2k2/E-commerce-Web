fb2bea9cb5cfdbb7fa2259ba1f590ea9172034bb
Ngo Duy Hoa <ngoduyhoaname2002@gmail.com>
2022-10-05 00:24:51 +0700

    Tui tải nhầm vào main m r

--- README.md
+++ README.md
@@ -4,6 +4,3 @@ XIN CHÀO🎆
 <hr>
 <h1>Có quà nè 🎁</h1>
 Dự án viết Spring + angular
-
-#Other versions
-[Another link](https://drive.google.com/drive/folders/1ZIKCk5TmzG2e32TNBFQgDZLbtNz4b-Hj?usp=sharing)
--- src/main/java/rrs/model/repositories/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package rrs.model.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import rrs.model.entities.Account;
-
-@Repository
-public interface AccountRepository extends JpaRepository<Account, String> {}
