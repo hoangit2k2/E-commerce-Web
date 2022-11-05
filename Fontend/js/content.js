7060e49e4660fe5d7afcc088d41d915a2087b33a
RoyalNguyen(BED16) <89145285+hoangit2k2@users.noreply.github.com>
2022-11-05 17:10:07 +0700

    update databse & fontend

--- Fontend/js/content.js
+++ Fontend/js/content.js
@@ -14,47 +14,120 @@ app.controller('controlconten', function($scope, $http, $routeParams) {
 	}
 	$scope.get();
 })
-app.directive('fileModel', ['$parse', function($parse){
-	return {
-		restrict: 'A',
-		link: function(scope,element,attrs){
-			var model = $parse(attrs.fileModel);
-			var modelSetter = model.assign;
 
-			element.bind('change', function(){
-				scope.$apply(function(){
-					modelSetter(scope, element[0].files[0]);
-				})
-			})
-		}
-	}
-}])
-app.service('multipartForm', ['$http', function($http){
-	this.post = function(uploadUrl, data){
-		var fd = new FormData();
-		for(var key in data){
-			fd.append(key, data[key]);
-		$http.post(uploadUrl, fd, {
-			TransformRequest: angular.indentity,
-			headers: {'Content-type': undefined}
-		})
-		}
-		console.log(data)
-	}
-}])
+app.controller('mypostcontroll', function($scope, $http){
+	$scope.form = [];
+	$scope.files = [];
+	$scope.insert = function() {
+		$scope.image1 = $scope.files[0];
+		$scope.image2 = $scope.files2[0];
+		$scope.image3 = $scope.files3[0];
 
-app.controller('submitController', ['$scope','$http', 'multipartForm', function($http, $scope, multipartForm){
-	$scope.customer = {};
-	var fd = new FormData();
-	this.post = function(uploadUrl, data){
-		for(var key in data){
-			fd.append(key, data[key]);
-		$http.post(uploadUrl, fd, {
-			TransformRequest: angular.indentity,
-			headers: {'Content-type': undefined}
-		})
-		}
+			$http({
+				method:'POST',
+				url:"http://localhost:8080/rest/contents/",
+				processData:false,
+				transformRequest:function(data){
+					var formData=new FormData();
+					formData.append("files", $scope.image1);
+					formData.append("files", $scope.image2);
+					formData.append("files", $scope.image3);
+					formData.append("customerid", $scope.customerid);
+					formData.append("categoryid", $scope.categoryid);
+					formData.append("namecontent", $scope.namecontent);
+					formData.append("subject", $scope.subject);
+					formData.append("price", $scope.price);
+					formData.append("email", $scope.email);
+					formData.append("phone", $scope.phone);
+					formData.append("address", $scope.address);
+
+
+
+				return formData;
+				return $scope.customerid;
+				return $scope.categoryid;
+				return $scope.namecontent;
+				return $scope.subject;
+				return $scope.price;
+				return $scope.email;
+				return $scope.phone;
+				return $scope.address;
+
+			},
+			data : $scope.form,
+			headers: {
+				'Content-Type': undefined
+			}
+		}).then(function(response){
+			var data = response.data
+			alert('Thêm thành công')
+		});
+	} 
+	//upload file 1
+	$scope.uploadedFile = function(element){
+		$scope.currentFile = element.files[0];
+		var reader = new FileReader();
+		
+		reader.onload = function(event){
+			var output = document.getElementById('output');
+			output.src = URL.createObjectURL(element.files[0]);
+			
+		$scope.image_source = event.target.result
+		$scope.$apply(function($scope){
+			$scope.files = element.files;
+		});
 	}
+		reader.readAsDataURL(element.files[0]);
+	}
+
+	$scope.uploadedFile2 = function(element){
+		$scope.currentFile2 = element.files[0];
+		var reader = new FileReader();
+		
+		reader.onload = function(event){
+			var output1 = document.getElementById('output1');
+			output1.src = URL.createObjectURL(element.files[0]);
+			
+		$scope.image_source = event.target.result
+		$scope.$apply(function($scope){
+			$scope.files2 = element.files;
+		});
+	}
+		reader.readAsDataURL(element.files[0]);
+	}
+	$scope.uploadedFile3 = function(element){
+		$scope.currentFile3 = element.files[0];
+		var reader = new FileReader();
+		
+		reader.onload = function(event){
+			var output2 = document.getElementById('output2');
+			output2.src = URL.createObjectURL(element.files[0]);
+			
+		$scope.image_source = event.target.result
+		$scope.$apply(function($scope){
+			$scope.files3 = element.files;
+		});
+	}
+		reader.readAsDataURL(element.files[0]);
+	}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	// $scope.uploadFiles = function() {
 	// 	var request = {
@@ -72,7 +145,7 @@ app.controller('submitController', ['$scope','$http', 'multipartForm', function(
 	// 	multipartForm.post(uploadUrl, data);
 	// }
 
-}])
+// }])
 
 
 
---
+++ database lovepink.sql
@@ -0,0 +1,94 @@
+﻿use master
+create database LovePink
+use LovePink
+create table users(
+	id int identity primary key,
+	username varchar(100),
+	password varchar(200),
+)
+create table staff(
+	id int identity,
+	userid int,
+	name nvarchar(100),
+	addres nvarchar(100),
+	email varchar(100),
+	phone varchar(100),
+	image varchar(500),
+	role bit,
+	foreign key (userid) references users(id)
+)
+create table customer(
+	id int identity primary key,
+	userid int,
+	name nvarchar(100),
+	address nvarchar(100),
+	phone varchar(15),
+	email varchar(100),
+	image varchar(200),
+	foreign key (userid) references users(id)
+)
+create table category(
+	id int identity primary key,
+	name nvarchar(50),
+	image varchar(100)
+)
+create table contents(
+	id int identity primary key,
+	customerid int,
+	categoryid int,
+	namecontent nvarchar(100),
+	subject nvarchar(500),
+	price int,
+	email varchar(100),
+	phone varchar(15),
+	address nvarchar(100),
+	datetime datetime,
+	status bit,
+	foreign key (customerid) references customer(id),
+	foreign key (categoryid) references category(id)
+)
+create table detailcontent(
+	id int identity,
+	contentid int,
+	origin nvarchar(100),
+	companyname nvarchar(100),
+	statusproduct nvarchar(50),
+	yearmanufacture nvarchar(100),
+	parameter nvarchar(50),
+	foreign key (contentid) references contents(id)
+)
+create table image (
+	contentid int ,
+	image varchar(100),
+	foreign key (contentid) references contents(id)
+)
+create table about(
+	id int identity primary key,
+	logo varchar(100),
+)
+create table banner(
+	aboutid int,
+	image varchar(100),
+	foreign key (aboutid) references about(id)
+)
+
+
+
+/**Create databse**/
+use lovepink
+insert into users values('hoang','123')
+insert into users values('hoa','123')
+insert into users values('thinh','123')
+insert into users values('quoc','123')
+insert into users values('nhan','123')
+
+insert into customer values(1,'Nguyen ba hoang', '321 lê đức thọ, gò vấp, TPHCM', '084859124','hoang@gmail.com','hoang.jpg')
+insert into customer values(2,'Ngo Duy Hoa', '321 phan van trị, bình thạnh, TPHCM', '084459124','hoa@gmail.com','hoa.jpg')
+insert into customer values(3,'Hồ Nguyên Quốc', '321 trường chinh, Bình tân, TPHCM', '084259124','quoc@gmail.com','quoc.jpg')
+insert into customer values(4,'Bùi quốc Thịnh', '123 Phạm văn đồng, Thủ đức, TPHCM', '084859124','thinh@gmail.com','thinh.jpg')
+insert into customer values(5,'Ca Thành Nhân', '11 lê đức thọ, quận 12, TPHCM', '084859124','nhan@gmail.com','nhan.jpg')
+
+insert into category values('điện thoại','phone.jpg')
+insert into contents values(1, 1, 'Dư điện thoại Iphone 12','Hiện tại không dùng đến nữa nên muốn bán đi với giá rẻ',12000000,'094124662', 'hoang@gmail.com','213, Lê Đức Thọ, gò vấp, HCM', '2020-10-30 10:07:11', 'true')
+
+delete from contents where id = 1
